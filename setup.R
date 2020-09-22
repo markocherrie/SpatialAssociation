7350db37234ccbe41257e714ef7b45c5d4e944a3
Mark <mark.cherrie.mc@gmail.com>
2020-09-22 12:08:34 +0100

    devtools package update

--- setup.R
+++ setup.R
@@ -1,9 +1,11 @@
-# load most the packages required
-load.lib<-c("sf", "dplyr", "pscl", "ggplot2", "MASS","devtools",
+# run below once
+library(devtools)
+devtools::install_github("michaeldorman/geobgu")
+
+# load all the packages required
+load.lib<-c("sf", "dplyr", "pscl", "ggplot2", "MASS","geobgu",
             "spdep", "lctools", "stars", "mapview", "leafsync")
 install.lib<-load.lib[!load.lib %in% installed.packages()]
 for(lib in install.lib) install.packages(lib,dependencies=TRUE)
 sapply(load.lib,require,character=TRUE)
 
-# run below once
-#devtools::install_github("michaeldorman/geobgu")
