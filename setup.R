409a6087cccebe751d38582ec2c2022ce39d661d
Mark <mark.cherrie.mc@gmail.com>
2020-09-22 11:41:54 +0100

    remove old files

--- setup.R
+++
@@ -1,6 +0,0 @@
-#setup
-
-load.lib<-c("readr", "sf", "dplyr", "tmap", "sp", "spdep")
-install.lib<-load.lib[!load.lib %in% installed.packages()]
-for(lib in install.lib) install.packages(lib,dependencies=TRUE)
-sapply(load.lib,require,character=TRUE)
